a4c86f096bec1658450d1416cb9d00decafe2536
userxld <keromvp@gmail.com>
2023-10-03 15:31:55 +0300

    refactored v2

--- homework-4/core/app/src/main.cpp
+++ homework-4/core/app/src/main.cpp
@@ -2,6 +2,7 @@
 #include <exampleLib/example_lib.hpp>
 #include <iostream>
 #include <list>
+#include <type_traits>
 
 #ifdef _VERSIONING
 #include "version.h"
@@ -14,69 +15,36 @@
 #endif
 
 using namespace std;
-/*
-namespace xxx
-{
-template <typename T> struct type_is
-{
-    using type = T;
-};
-// base
-template <bool cond, typename T> struct enable_if : type_is<T>
-{
-};
-
-template <typename T> struct enable_if<false, T>;
-} // namespace xxx
-
-// invalid for vector
-namespace xxx
-{
-template <typename T> auto generic_sort(T& container, int) -> decltype(container.sort())
-{
-    container.sort();
-}
-template <typename T> void generic_sort(T& container, long)
-{
-    std::sort(container.begin(), container.end());
-}
-} // namespace xxx
-template <typename T> auto generic_sort(T& container)
-{
-    xxx::generic_sort(container, 42);
-}
-*/
 namespace sorting
 {
 
-template <typename T> typename std::enable_if_t<std::is_same_v<T, int8_t>, void> print_ip(T generic_ip)
+template <typename T> typename std::enable_if_t<std::is_integral_v<T>, void> print_ip(T number)
 {
-    cout << "uint8" << endl;
+    unsigned short mask = 0xFF;
+    for (auto index = sizeof(T) - 1; index > 0; index--)
+    {
+        cout << ((number >> index * 8) & mask) << '.';
+    }
+
+    cout << (number & mask) << endl;
+}
+template <typename T> typename std::enable_if_t<std::is_same_v<T, std::string>, void> print_ip(T number)
+{
+    cout << number << endl;
 }
 
-template <typename T> typename std::enable_if_t<std::is_same_v<T, int16_t>, void> print_ip(T generic_ip)
+template <typename C>
+decltype(begin(declval<C>()), end(declval<C>()), typename enable_if<!is_same_v<string, C>, void>::type()) print_ip(
+    const C& container)
 {
-    cout << "uint16" << endl;
-}
-template <typename T> typename std::enable_if_t<std::is_same_v<T, int32_t>, void> print_ip(T generic_ip)
-{
-    cout << "uint32" << endl;
-}
-template <typename T> typename std::enable_if_t<std::is_same_v<T, std::vector<int>>, void> print_ip(T generic_ip)
-{
-    cout << "vector" << endl;
-    for (auto const i : generic_ip)
+    string separator = "";
+    for (const auto part : container)
     {
-        cout << i << endl;
+        cout << separator << part;
+        separator = ".";
     }
-}
-template <typename T> typename std::enable_if_t<std::is_same_v<T, std::list<short>>, void> print_ip(T generic_ip)
-{
-    cout << "list" << endl;
-    for (auto const i : generic_ip)
-    {
-        cout << i << endl;
-    }
+
+    cout << endl;
 }
 
 } // namespace sorting
@@ -89,11 +57,13 @@ template <typename U> void print_ip(U generic_ip)
 int main(int argc, char* argv[])
 {
     sorting::print_ip(int8_t{-1});
-    sorting::print_ip(int16_t{55});
-    sorting::print_ip(int32_t{555});
-    std::vector v1{1, 2, 34, 4};
-    std::list<short> v2{123, 323, 434, 323};
+    sorting::print_ip(int16_t{-1});
+    sorting::print_ip(long{8875824491850138409});
+    sorting::print_ip("dasdasdada"s);
+    std::list<short> v1{1, 23, 4, 543};
+    sorting::print_ip("dasdasdada"s);
     sorting::print_ip(v1);
-    sorting::print_ip(v2);
+    int x = 10;
+    decltype(x, y) x2 = 10;
     return EXIT_SUCCESS;
 }
