40164c2bdb39674fa3399a11f0bde4fa20bda3ad
Kerosin3 <keromvp@gmail.com>
2023-11-13 22:14:08 +0300

    added simple versio

--- homework-3/core/app/src/main.cpp
+++ homework-3/core/app/src/main.cpp
@@ -1,7 +1,6 @@
 #include "exampleLib/example_lib.hpp"
 #include <iostream>
 #include <list>
-#include <memory>
 #include <type_traits>
 #include <vector>
 
@@ -14,13 +13,46 @@
 #elif __cplusplus < 202002L
 #error C++20 is required
 #endif
-
+// https://howardhinnant.github.io/allocator_boilerplate.html
 using namespace std;
+
+template <typename T> class Xallocator
+{
+  public:
+    using value_type = T;
+
+    Xallocator() noexcept {} // not required, unless used
+    template <typename U> Xallocator(Xallocator<U> const&) noexcept {}
+
+    value_type* // Use pointer if pointer is not a value_type*
+    allocate(std::size_t n)
+    {
+        cout << "allocation" << endl;
+        return static_cast<value_type*>(::operator new(n * sizeof(value_type)));
+    }
+
+    void deallocate(value_type* p, std::size_t) noexcept // Use pointer if pointer is not a value_type*
+    {
+        cout << "deallocation" << endl;
+        ::operator delete(p);
+    }
+
+};
+
+template <typename T, typename U> bool operator==(Xallocator<T> const&, Xallocator<U> const&) noexcept
+{
+    return true;
+}
+
+template <typename T, typename U> bool operator!=(Xallocator<T> const& x, Xallocator<U> const& y) noexcept
+{
+    return !(x == y);
+}
 
 int main(int argc, char* argv[])
 {
-    auto x = func1(100, 100);
-
-    delete x;
+    std::vector<int,Xallocator<int>> xvec;
+    xvec.push_back(5);
+    xvec.push_back(5);
     return EXIT_SUCCESS;
 }
