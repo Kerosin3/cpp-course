64946f0bde1b5bc83aa27f896e90925065909d47
userxld <keromvp@gmail.com>
2023-11-14 17:59:35 +0300

    added buffer

--- homework-3/core/app/src/main.cpp
+++ homework-3/core/app/src/main.cpp
@@ -1,6 +1,10 @@
 #include "exampleLib/example_lib.hpp"
+#include <cstddef>
 #include <iostream>
 #include <list>
+#include <map>
+#include <new>
+#include <stdexcept>
 #include <type_traits>
 #include <vector>
 
@@ -16,43 +20,76 @@
 // https://howardhinnant.github.io/allocator_boilerplate.html
 using namespace std;
 
-template <typename T> class Xallocator
+template <typename T, size_t B_size>
+class Buffer
+{
+
+  public:
+    using size_type = decltype(B_size);
+    using value_type = T;
+    using reference = const T&;
+    using pointer = const T*;
+
+  private:
+    pointer head_data;
+
+  public:
+    Buffer()
+    {
+        head_data = (pointer)malloc(sizeof(value_type) * B_size);
+
+        if (!head_data)
+            throw std::bad_alloc();
+    }
+    virtual ~Buffer()
+    {
+        cout << "deleting" << endl;
+        delete head_data;
+    };
+    reference operator[](size_type index) const
+    {
+        if (index >= B_size || index < 0)
+            throw std::out_of_range("buffer index is out of range");
+        return *(head_data + index);
+    }
+};
+
+template <typename T, size_t SIZE>
+class Xallocator
 {
   public:
     using value_type = T;
+    using c_reference = const T&;
+    using c_pointer = const T*;
+    using reference = T&;
+    using pointer = T*;
+    using self = Xallocator<T, SIZE>;
+    using size_type = decltype(SIZE);
 
-    Xallocator() noexcept {} // not required, unless used
-    template <typename U> Xallocator(Xallocator<U> const&) noexcept {}
-
-    value_type* // Use pointer if pointer is not a value_type*
-    allocate(std::size_t n)
+    template <typename A>
+    struct rebind
     {
-        cout << "allocation" << endl;
-        return static_cast<value_type*>(::operator new(n * sizeof(value_type)));
+        using other = Xallocator<A, SIZE>;
+    };
+    ~Xallocator() = default;
+    Xallocator(const self& other) = delete;
+    Xallocator(self&& other) = delete;
+    Xallocator operator=(self) = delete;
+    Xallocator() : m_allocator_buffer(Buffer<value_type, SIZE>()), m_offset(size_type()) {}
+    void allocate(size_t count){
+      if (count >= SIZE)
+        throw std::bad_alloc();
+      size_type old_offset = m_offset;
+      ++m_offset;
+      return &m_allocator_buffer[m_offset];
     }
-
-    void deallocate(value_type* p, std::size_t) noexcept // Use pointer if pointer is not a value_type*
-    {
-        cout << "deallocation" << endl;
-        ::operator delete(p);
-    }
-
+  private:
+    Buffer<value_type, SIZE> m_allocator_buffer;
+    size_type m_offset{
 };
-
-template <typename T, typename U> bool operator==(Xallocator<T> const&, Xallocator<U> const&) noexcept
-{
-    return true;
-}
-
-template <typename T, typename U> bool operator!=(Xallocator<T> const& x, Xallocator<U> const& y) noexcept
-{
-    return !(x == y);
-}
 
 int main(int argc, char* argv[])
 {
-    std::vector<int,Xallocator<int>> xvec;
-    xvec.push_back(5);
-    xvec.push_back(5);
+  i
     return EXIT_SUCCESS;
 }
