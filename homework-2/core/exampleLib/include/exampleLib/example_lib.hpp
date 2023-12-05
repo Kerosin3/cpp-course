b1c3b7645f0bdd856289f8f3101b8193bac71b23
Kerosin3 <keromvp@gmail.com>
2023-12-05 14:16:38 +0300

    Revert "remove for review2_my"
    This reverts commit be9eda84910dc070a9ea22e6b2b9b0241fb23191.

---
+++ homework-2/core/exampleLib/include/exampleLib/example_lib.hpp
@@ -0,0 +1,61 @@
+#ifndef EXAMPLE_LIB
+#define EXAMPLE_LIB
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cstdint>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+#endif
+
+#ifdef _VERSIONING
+#include "version.h"
+#endif
+
+enum class BytePlace : unsigned
+{
+    forth = 0x1,
+    third = 0x2,
+    second = 0x4,
+    first = 0x8,
+    any = 0xf
+};
+
+class Filtering
+{
+    std::istream& cinstream;
+    std::vector<uint32_t> input_lines;
+    std::vector<uint32_t> tmp_storage;
+    bool sequence{false};
+
+  public:
+    Filtering() = delete;
+    Filtering(std::istream& istream) : cinstream(istream){};
+    void read_input();
+    inline static void sort_descending(std::vector<uint32_t>&);
+    inline static std::string get_ip_char_repr(uint32_t);
+    inline static uint32_t get_ipv4_int(const std::string&);
+    void printout();
+    void filter_this(uint8_t, BytePlace);
+    inline void reset_sequence()
+    {
+        this->tmp_storage.clear();
+        this->sequence = false;
+    }
+    static constexpr size_t mc_string_size = 17;
+};
+
+// comparing ipv4 addresses as uint32's
+bool compare_ip_as_numbers(uint32_t, uint32_t);
+
+// true if specified byte position matches specified byte value
+bool filter_by_byte(uint32_t, uint8_t byte, BytePlace place);
+
+// convert ipv4 string with . delimiter to hex representation
+uint32_t convert(const std::string& ipv4Str);
