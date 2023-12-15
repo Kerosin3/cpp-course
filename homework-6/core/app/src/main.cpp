d8ddf2f80695b63580f00ddb198900a461b36312
Kerosin3 <keromvp@gmail.com>
2023-12-15 17:14:50 +0300

    hw6 first

--- homework-6/core/app/src/main.cpp
+++ homework-6/core/app/src/main.cpp
@@ -1,3 +1,4 @@
+#include <concepts>
 #include <cstddef>
 #include <iostream>
 #include <list>
@@ -15,12 +16,65 @@
 #ifndef __cplusplus
 error C++ is required elif __cplusplus < 202002L error C++ 20 is required
 #endif
-
-    // https://www.bogotobogo.com/DesignPatterns/mvc_model_view_controller_pattern.php
     using namespace std;
+
+// template <typename U>
+// concept NonPointer = not is_pointer_v<U>;
+
+template <typename U, U d_val>
+requires std::integral<U>
+class Field
+{
+  public:
+    Field() = default;
+    virtual ~Field() = default;
+
+    Field& operator=(U rhs)
+    {
+        m_value = rhs;
+        return *this; // ?
+    }
+    operator U() const
+    {
+        return m_value;
+    }
+
+  private:
+    U m_value;
+};
+
+template <typename U, U d_val>
+requires std::integral<U>
+class Cell
+{
+  public:
+    Cell() = default;
+    virtual ~Cell() = default;
+
+  private:
+    // store map to cells
+    static std::map<U, Field<U, d_val>> m_cell_map;
+};
+
+template <typename U, U d_val>
+requires std::integral<U>
+class AMatrix
+{
+  public:
+    AMatrix() = default;
+    virtual ~AMatrix() = default;
+    size_t get_size() const
+    {
+        return 0;
+    }
+
+  private:
+    // store map to cells
+    static std::map<U, Cell<U, d_val>> m_data_matrix;
+};
+
 int main(int argc, char* argv[])
 {
     cout << "program starts" << endl;
-    // просто прямоугольник 100 на 100
     return EXIT_SUCCESS;
 }
