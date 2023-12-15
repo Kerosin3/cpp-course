43f4db6526d90b2c330d85d19e3dc6f13c489d51
Kerosin3 <keromvp@gmail.com>
2023-12-15 16:18:24 +0300

    hw6 initial

---
+++ homework-6/core/app/src/main.cpp
@@ -0,0 +1,26 @@
+#include <cstddef>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <stdexcept>
+#include <type_traits>
+#include <typeinfo>
+#include <utility>
+#include <vector>
+#ifdef _VERSIONING
+#include "version.h"
+#endif
+
+#ifndef __cplusplus
+error C++ is required elif __cplusplus < 202002L error C++ 20 is required
+#endif
+
+    // https://www.bogotobogo.com/DesignPatterns/mvc_model_view_controller_pattern.php
+    using namespace std;
+int main(int argc, char* argv[])
+{
+    cout << "program starts" << endl;
+    // просто прямоугольник 100 на 100
+    return EXIT_SUCCESS;
+}
